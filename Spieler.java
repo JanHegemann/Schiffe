8287b5c01a322e809fae028494d9c627bf848531
Jan <janhegemann1@gmail.com>
2018-07-06 09:02:16 +0200

    h

--- Spieler.java
+++ Spieler.java
@@ -8,13 +8,32 @@ public class Spieler
     // Bezugsobjekte
 
     // Attribute
-    String id, name, password;
+    private String id, name, password, clientIp;
+    private int clientPort;
     // Konstruktor
-    public Spieler(String id, String name, String password)
+    public Spieler(String pId, String pname, String ppassword,String pClientIp, int pClientPort)
     {
-        
+        this.id=pId;
+        this.name=pname;
+        this.password=ppassword;
+        this.clientIp=pClientIp;
+        this.clientPort=pClientPort;
     }
 
     // Dienste
-
+    public String gibId (){
+        return this.id;
+    }
+    public String gibName (){
+        return this.name;
+    }
+    public String gibPassword (){
+        return this.password;
+    }
+    public String gibClientIP (){
+        return this.clientIp;
+    }
+    public int gibClientPort (){
+        return this.clientPort;
+    }
 }
